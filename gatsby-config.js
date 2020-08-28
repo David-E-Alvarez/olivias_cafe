6d999331f9bbaa80c4f974d8e53f9a2f9e6562cf
David Alvarez <davidealvarez1@gmail.com>
2020-08-28 10:31:03 -0700

    gutting code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Olivia's Cafe`,
+    description: `Olivia's Cafe Mexican Food Restaurant`,
     author: `@gatsbyjs`,
   },
   plugins: [
