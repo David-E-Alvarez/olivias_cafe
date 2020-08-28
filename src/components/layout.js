a088975e540a278582fe0aad4de1ff46c16a7cca
David Alvarez <davidealvarez1@gmail.com>
2020-08-28 11:23:34 -0700

    starting from the bottom

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,21 +11,12 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout = () => {
 
 
   return (
     <>
       <Header />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
     </>
   )
 }
